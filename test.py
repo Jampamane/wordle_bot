6e873bed23620bd284008f10b6056cddf88db4fe
Jampamane <68441282+Jampamane@users.noreply.github.com>
2024-05-06 21:10:43 +0000

    modified:   five_letter_words.json 	modified:   main.py 	modified:   test.py 	modified:   wordle.py

--- test.py
+++ test.py
@@ -1,11 +1,2 @@
-import requests
-from bs4 import BeautifulSoup
-from rich.console import Console
-
-NYT_WORDLE = "https://www.nytimes.com/games/wordle/index.html"
-
-response = requests.get(NYT_WORDLE)
-soup = BeautifulSoup(response.content, "html.parser")
-console = Console()
-
-console.print(soup.prettify())
+for indx in range(1, 6):
+    print(indx)
--- wordle.py
+++ wordle.py
@@ -3,6 +3,9 @@ import json
 import random
 import os
 
+from rich.live import Live
+from rich.table import Table
+
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
@@ -28,7 +31,7 @@ class Wordle():
         FIVE_LETTER_WORDS = json.load(file)
 
     def __init__(self) -> None:
-        self.potential_words = []
+        self.potential_words = self.FIVE_LETTER_WORDS
         self.wordle = {
             "letters": {
                 1 : {"correct": "", "incorrect": []},
@@ -39,10 +42,11 @@ class Wordle():
             },
             "absent_letters": [],
             "present_letters": []}
+        self.table = self.build_table()
         options = Options()
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
         options.add_argument('--log-level=3')
-        #options.add_argument('--headless')
+        options.add_argument('--headless')
         options.add_argument('--ignore-certificate-errors')
         options.add_argument('--ignore-ssl-errors')
         self.browser = Chrome(options=options)
@@ -56,6 +60,15 @@ class Wordle():
         self.action_chains.send_keys(Keys.ESCAPE).perform()
         time.sleep(1)
 
+    def build_table(self) -> Table:
+        table = Table(title="Wordle")
+        table.add_column("Guess", justify="center")
+        table.add_column("1")
+        table.add_column("2")
+        table.add_column("3")
+        table.add_column("4")
+        table.add_column("5")
+        return table
 
     def submit_guess(self, guess: str, row_number: int) -> bool:
         for letter in guess:
@@ -65,16 +78,19 @@ class Wordle():
         time.sleep(3)
 
         row = self.browser.find_elements(By.CLASS_NAME, self.ROW_CLASS)[row_number - 1]
-        empty = row.find_elements(By.CSS_SELECTOR, self.CSS_EMPTY)
-        if len(empty) == 0:
-            return True
-        return False
+        tiles = row.find_elements(By.CLASS_NAME, self.TILE_CLASS)
+        for letter in tiles:
+            letter_check = letter.get_attribute("data-state")
+            if letter_check == "tbd":
+                return False
+        return True
+
 
     def update_wordle(self, row_number: int) -> None:
         row = self.browser.find_elements(By.CLASS_NAME, self.ROW_CLASS)[row_number - 1]
         tiles = row.find_elements(By.CLASS_NAME, self.TILE_CLASS)
         for indx, letter in enumerate(tiles, start=1):
-            letter_check = letter.get_dom_attribute("data-state")
+            letter_check = letter.get_attribute("data-state")
             if letter_check == "absent":
                 self.wordle["absent_letters"].append(letter.text.lower())
             elif letter_check == "correct":
@@ -83,9 +99,36 @@ class Wordle():
                 self.wordle["present_letters"].append(letter.text.lower())
                 self.wordle["letters"][indx]["incorrect"].append(letter.text.lower())
 
+
+    def update_table(self, row_number: int):
+        style_dict = {
+            "absent": "grey53",
+            "present": "yellow bold",
+            "correct": "green bold",
+            "tbd": "red"
+            }
+        table_list = []
+        table_list.append((row_number, "cyan bold"))
+        row = self.browser.find_elements(By.CLASS_NAME, self.ROW_CLASS)[row_number - 1]
+        tiles = row.find_elements(By.CLASS_NAME, self.TILE_CLASS)
+        for letter in tiles:
+            letter_check = letter.get_attribute("data-state")
+            table_list.append((letter.text, style_dict[letter_check]))
+
+        self.table.add_row(
+            f"[{table_list[0][1]}]{table_list[0][0]}",
+            f"[{table_list[1][1]}]{table_list[1][0]}",
+            f"[{table_list[2][1]}]{table_list[2][0]}",
+            f"[{table_list[3][1]}]{table_list[3][0]}",
+            f"[{table_list[4][1]}]{table_list[4][0]}",
+            f"[{table_list[5][1]}]{table_list[5][0]}",
+        )
+        self.table.add_section()
+
+
     def get_potential_words(self):
         potential_words = {}
-        for word in self.FIVE_LETTER_WORDS.keys():
+        for word in self.potential_words.keys():
             for indx, letter in enumerate(word, start=1):
                 if letter in self.wordle['absent_letters']:
                     break
@@ -107,10 +150,115 @@ class Wordle():
                         potential_words[word] = 1
         return potential_words
 
+
     def check_for_win(self):
         for indx in range(1, 6):
-            if self.wordle['letters'][indx]['correct']:
-                pass
-            else:
+            if not self.wordle['letters'][indx]['correct']:
                 return False
         return True
+
+
+    def delete_guess(self, guess: str):
+        for _ in range(6):
+            self.action_chains.send_keys(Keys.BACK_SPACE).perform()
+            time.sleep(0.1)
+        try:
+            self.potential_words.pop(guess)
+        except KeyError:
+            pass
+        self.FIVE_LETTER_WORDS.pop(guess)
+
+        with open(self.FIVE_LETTER_WORDS_ABSOLUTE_PATH, 'w') as file:
+            file.write(json.dumps(self.FIVE_LETTER_WORDS, indent=1))
+
+
+    def _solve_hard(self, first_guess: str) -> bool:
+        with Live(self.table) as live:
+            if self.submit_guess(guess=first_guess, row_number=1) is False:
+                raise ValueError("You have provided an invalid first guess.")
+            self.update_wordle(row_number=1)
+            self.update_table(row_number=1)
+            live.update(self.table)
+            for indx in range(2, 7):
+                if self.check_for_win() is True:
+                    return True
+                self.potential_words = self.get_potential_words()
+                new_guess = random.choice(list(self.potential_words.keys()))
+                print(new_guess)
+                while self.submit_guess(guess=new_guess, row_number=indx) is False:
+                    print(f"{new_guess} is bad guess")
+                    self.delete_guess(new_guess)  
+                    new_guess = random.choice(list(self.potential_words.keys()))
+                    print(new_guess)
+                self.update_wordle(row_number=indx)
+                self.update_table(row_number=indx)
+                live.update(self.table)
+
+        if self.check_for_win() is True:
+                return True
+        return False
+    
+    def _easy_mode_get_guess(self):
+        good_guesses = []
+        availible_letters = {}
+        for word in self.potential_words.keys():
+            for letter in word:
+                availible_letters[letter] = 1
+
+        for indx in range(1, 6):
+            try:
+                availible_letters.pop(self.wordle["letters"][indx]["correct"])
+            except KeyError:
+                pass
+
+        for word in self.FIVE_LETTER_WORDS:
+            for index, letter in enumerate(word, start=1):
+                if letter in self.wordle["letters"][index]["incorrect"]:
+                    break
+
+                if letter not in list(availible_letters.keys()):
+                    break
+
+                good_guesses.append(word)
+
+        try:
+            guess = random.choice(good_guesses)
+        except IndexError:
+            guess = random.choice(list(self.potential_words.keys()))
+        return guess
+
+            
+
+        
+    
+    def _solve_easy(self, first_guess: str):
+         with Live(self.table) as live:
+            if self.submit_guess(guess=first_guess, row_number=1) is False:
+                raise ValueError("You have provided an invalid first guess.")
+            self.update_wordle(row_number=1)
+            self.update_table(row_number=1)
+            live.update(self.table)
+            for indx in range(2, 7):
+                if self.check_for_win() is True:
+                    return True
+                self.potential_words = self.get_potential_words()
+                new_guess = self._easy_mode_get_guess()
+                print(new_guess)
+                while self.submit_guess(guess=new_guess, row_number=indx) is False:
+                    print(f"{new_guess} is bad guess")
+                    self.delete_guess(new_guess)  
+                    new_guess = self._easy_mode_get_guess()
+                    print(new_guess)
+                self.update_wordle(row_number=indx)
+                self.update_table(row_number=indx)
+                live.update(self.table)
+    
+    def solve(self, mode: str="hard", first_guess: str="arose"):
+        if mode == "hard":
+            win = self._solve_hard(first_guess=first_guess)
+            if win is True:
+                print("You win!")
+            else:
+                print("You lose...")
+        elif mode == "easy":
+            self._solve_easy(first_guess=first_guess)
