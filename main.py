59646a9e488118287d1376b8e46c743599a7dbab
Jampamane <68441282+Jampamane@users.noreply.github.com>
2024-05-08 20:43:02 +0000

    modified:   five_letter_words.json 	modified:   main.py

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from wordle import Wordle
 
 
 def main():
-    wordle = Wordle()
+    wordle = Wordle(headless=True)
     wordle.solve()
 
 if __name__ == "__main__":
-    main()
+    while True:
+        main()
