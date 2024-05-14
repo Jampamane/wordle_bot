9fdeb32a9e628c1f5e341905cd80ea912e129f11
Jeremy Brandow <jampamane@gmail.com>
2024-05-14 07:43:47 -0400

    Added docstrings

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
-from rich.console import Console
+"""Main function that handles creating an instance and solving wordle."""
 from wordle import Wordle
-from datetime import datetime
 
 
 def main():
-    console = Console(record=True)
+    """Create a wordle class and calls the solve method."""
     wordle = Wordle(headless=True)
-    console.print("\n")
-    if wordle.solve() == True:
-        console.clear()
-        console.print(wordle._build_table())
-        console.print(f"\n- Today's wordle is:  [green bold]{wordle.wordle_today.upper()}[/green bold] -\n")
+    wordle.solve()
 
-    with open("wordle.html", "wt") as file:
-        file.write(console.export_html())
 
 if __name__ == "__main__":
     main()
