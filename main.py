05ace93f46856b4c7c7e708658f3940c2145a341
Jampamane <68441282+Jampamane@users.noreply.github.com>
2024-07-09 19:55:25 +0000

    table formatting

--- main.py
+++ main.py
@@ -24,20 +24,20 @@ def main():
             if not len(wordle.wordle["guess"][x]["word"]) == 0:
                 final_table.print(
                     "   * - " + wordle.wordle["guess"][x]["word"] + "\n" +
-                    "   - " + wordle.wordle["guess"][x]["letters"][0][0] + "\n" +
-                    "   - " + wordle.wordle["guess"][x]["letters"][1][0] + "\n" +
-                    "   - " + wordle.wordle["guess"][x]["letters"][2][0] + "\n" +
-                    "   - " + wordle.wordle["guess"][x]["letters"][3][0] + "\n" +
-                    "   - " + wordle.wordle["guess"][x]["letters"][4][0] + "\n"
+                    "     - " + wordle.wordle["guess"][x]["letters"][0][0] + "\n" +
+                    "     - " + wordle.wordle["guess"][x]["letters"][1][0] + "\n" +
+                    "     - " + wordle.wordle["guess"][x]["letters"][2][0] + "\n" +
+                    "     - " + wordle.wordle["guess"][x]["letters"][3][0] + "\n" +
+                    "     - " + wordle.wordle["guess"][x]["letters"][4][0]
                 )
             else:
                 final_table.print(
                     "   * - \n" +
-                    "   - \n" +
-                    "   - \n" +
-                    "   - \n" +
-                    "   - \n" +
-                    "   - \n"
+                    "     - \n" +
+                    "     - \n" +
+                    "     - \n" +
+                    "     - \n" +
+                    "     -"
                 )
         
         final_table.print(f"Today's wordle is: {wordle.wordle_today.upper()}")
