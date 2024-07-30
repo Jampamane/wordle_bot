77550fa9130571a4113d39eb28e3432d55786014
Jeremy Brandow <jampamane@gmail.com>
2024-07-30 16:50:43 +0000

    added argument parser and --export

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 """Main function that handles creating an instance and solving wordle."""
 
+from argparse import ArgumentParser
 from wordle import Wordle
 from output_file import output_file
 
 
-def main():
+def main(export):
     """Create a wordle class and calls the solve method."""
     # Solve wordle
     wordle = Wordle(headless=True)
     wordle.solve()
 
-    # Output to rst file
-    output_file(wordle=wordle)
+    if export is True:
+        # Output to rst file
+        output_file(wordle=wordle)
 
 
 if __name__ == "__main__":
-    main()
+    parser = ArgumentParser()
+    parser.add_argument("--export", default=False, action="store_true")
+    args = parser.parse_args()
+    main(export=args.export)
