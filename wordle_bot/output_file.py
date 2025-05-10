16ed9203e154a3232f30a8b3ce4565464b687af6
Jeremy Brandow <jampamane@gmail.com>
2025-05-10 08:03:10 -0400

    fix: fixing final table output in pages

--- wordle_bot/output_file.py
+++ wordle_bot/output_file.py
@@ -21,42 +21,26 @@ def output_file(wordle: Wordle) -> None:
         final_table = Console(file=file)
         today = date.today()
         final_table.print(
-            ".. list-table:: Wordle for "
-            + str(today)
+            "|  |  |  |  |  |  |"
             + "\n"
-            + "   :widths: 10 3 3 3 3 3\n"
-            + "   :header-rows: 0\n"
-            + "   :align: left\n"
+            + "|--|--|--|--|--|--|"
+            + "\n"
         )
         for x in range(1, 7):
             if not len(wordle.wordle["guess"][x]["word"]) == 0:
                 final_table.print(
-                    "   * - "
-                    + wordle.wordle["guess"][x]["word"].capitalize()
-                    + "\n"
-                    + "     - "
-                    + wordle.wordle["guess"][x]["letters"][0][0]
-                    + "\n"
-                    + "     - "
-                    + wordle.wordle["guess"][x]["letters"][1][0]
-                    + "\n"
-                    + "     - "
-                    + wordle.wordle["guess"][x]["letters"][2][0]
-                    + "\n"
-                    + "     - "
-                    + wordle.wordle["guess"][x]["letters"][3][0]
-                    + "\n"
-                    + "     - "
-                    + wordle.wordle["guess"][x]["letters"][4][0]
+                    f"|{wordle.wordle["guess"][x]["word"].capitalize()}|" +
+                    f"|{wordle.wordle["guess"][x]["letters"][1][0]}|" +
+                    f"|{wordle.wordle["guess"][x]["letters"][2][0]}|" +
+                    f"|{wordle.wordle["guess"][x]["letters"][0][0]}|" +
+                    f"|{wordle.wordle["guess"][x]["letters"][3][0]}|" +
+                    f"|{wordle.wordle["guess"][x]["letters"][4][0]}|" +
+                    "\n"
                 )
             else:
                 final_table.print(
-                    "   * - \n"
-                    + "     - \n"
-                    + "     - \n"
-                    + "     - \n"
-                    + "     - \n"
-                    + "     -"
+                    "|  |  |  |  |  |  |"
+                    + "\n"
                 )
 
-        final_table.print(f"Today's wordle is: {wordle.wordle_today.upper()}")
+        final_table.print(f"The Wordle for {today} is: {wordle.wordle_today.upper()}")
